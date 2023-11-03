0862dbc07a2d5afc8da906f5e31b648f761bd32a
Thomas Liu <tommyliu9@gmail.com>
2023-11-03 16:33:47 -0400

    fix: missing env

--- src/client/types/config.ts
+++ src/client/types/config.ts
@@ -4,8 +4,8 @@ export const config = {
     'Become the co-owner of the second Pop Art Wonder called "The Evolution of Art". This painting is a tribute to the 16 most popular artworks in history. The painting will be revealed over 16 days from October 20th to November 4th.',
   contractAddress: '0x8f3ad4bffa66ba9e689881c80e5309af78b70e10',
   openseaUrl: 'https://opensea.io/collection/popartwonders',
-  startDate: '2023/11/04',
-  endDate: '2023/11/14',
+  startDate: '2023-11-04T14:00:05.000Z',
+  endDate: '2023-11-14T12:00:05.000Z',
   twitter: {
     url: 'https://twitter.com/popartcats',
     handle: '@popartcats',
