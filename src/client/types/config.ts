eef5113bc0fea97849f5e0bcf6fe2ee62b8462ea
Thomas Liu <tommyliu9@gmail.com>
2023-11-03 17:26:36 -0400

    feat: switched to prpod

--- src/client/types/config.ts
+++ src/client/types/config.ts
@@ -2,7 +2,7 @@ export const config = {
   appName: 'The Evolution of Art',
   description:
     'Become the co-owner of the second Pop Art Wonder called "The Evolution of Art". This painting is a tribute to the 16 most popular artworks in history. The painting will be revealed over 11 days from October 20th to November 4th.',
-  contractAddress: '0x8f3ad4bffa66ba9e689881c80e5309af78b70e10',
+  contractAddress: '0xbba45e5b08233014fa676b1cd9345e58bda2582e',
   openseaUrl: 'https://opensea.io/collection/popartwonders',
   startDate: '2023-11-04T16:00:05.000Z',
   endDate: '2023-11-14T12:00:05.000Z',
