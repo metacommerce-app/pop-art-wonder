e162b0006336b1d84d54c1e4d57c5a5175b11bed
charlesashby <charlesa.l@hotmail.com>
2023-11-04 10:40:30 -0400

    fix: update start date

--- src/client/types/config.ts
+++ src/client/types/config.ts
@@ -4,7 +4,7 @@ export const config = {
     'Become the co-owner of the second Pop Art Wonder called "The Evolution of Art". This painting is a tribute to the 16 most iconic artworks in history. Each minted NFT represents a fraction of the 79x79 inches painting and makes you the co-owner of the physical piece.',
   contractAddress: '0xbba45e5b08233014fa676b1cd9345e58bda2582e',
   openseaUrl: 'https://opensea.io/collection/popartwonders',
-  startDate: '2023-11-04T16:00:04.000Z',
+  startDate: '2023-11-04T16:30:04.000Z',
   endDate: '2023-11-14T12:00:05.000Z',
   twitter: {
     url: 'https://twitter.com/popartcats',
