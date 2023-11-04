12386477527ea18ab2ee9a5fdb22e49d8a915be4
Thomas Liu <tommyliu9@gmail.com>
2023-11-04 12:20:08 -0400

    feat: updated allowlist

--- src/providers/nft/NftDetailsProvier.tsx
+++ src/providers/nft/NftDetailsProvier.tsx
@@ -14,6 +14,7 @@ interface NftDetailsProviderProps {
 }
 
 const NftDetailsProvider: React.FC<NftDetailsProviderProps> = ({ children }) => {
+  //
   const [totalMinted, setTotalMinted] = useState<number>(0);
   const { estimateContractGas, getGasPrice, readContract } = usePublicClient();
   const [gas, setGas] = useState<GasDetails>({});
