741010308835cc004bda1d45b4c1c769fa292027
charlesashby <charlesa.l@hotmail.com>
2023-11-14 12:22:16 -0500

    fix: close mint

--- src/client/home/MintSection/MintingModalButton/MintingModalButton.tsx
+++ src/client/home/MintSection/MintingModalButton/MintingModalButton.tsx
@@ -1,9 +1,8 @@
 import Button from '@/components/buttons/Button/Button';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import CheckoutModal from './MintingModal/MintingModal';
 import { FaArrowRight } from 'react-icons/fa';
-import { useMint } from '../../useMint';
 
 interface MintingButtonProps {
   className?: string;
@@ -21,12 +20,9 @@ const MintingButton: React.FC<MintingButtonProps> = ({ className }) => {
    */
   return (
     <>
-      <Button onClick={openModal} className={`group bg-pink-200 flex flex-row items-center p-4 w-full rounded-md hover:bg-gray-900 hover:text-white ${className}`}>
-        <div className='flex'>{t('Mint Now')}</div>
+      <Button disabled={true} className={`group bg-gray-700 text-white flex flex-row items-center p-4 w-full rounded-md  ${className}`}>
+        <div className='flex'>{t('Minting is closed')}</div>
         <div className='flex flex-grow'></div>
-        <div className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none text-xs'>
-          <FaArrowRight />
-        </div>
       </Button>
       <CheckoutModal open={open} onClose={closeModal} />
     </>
